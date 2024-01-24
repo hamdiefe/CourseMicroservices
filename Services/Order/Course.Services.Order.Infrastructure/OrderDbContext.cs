935b10be17115188012c36e4832fdc02ceab8614
hamdiefe <hamdi.efe@outlook.com>
2024-01-24 21:10:51 +0300

    OrderDbContext

---
+++ Services/Order/Course.Services.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Course.Services.Order.Domain.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Services.Order.Infrastructure
+{
+    public class OrderDbContext : DbContext
+    {
+        public const string DEFAULT_SCHEME = "ordering";
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Domain.OrderAggregate.Order> Order { get; set; }
+
+        public DbSet<Domain.OrderAggregate.OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().ToTable("Order", DEFAULT_SCHEME);
+            modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().ToTable("OrderItem", DEFAULT_SCHEME);
+
+            modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().Property(x => x.Price).HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().OwnsOne(x => x.Address).WithOwner();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
