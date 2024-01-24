76d0b1fa63bb591d387c63e876f080d57b85ddb1
hamdiefe <hamdi.efe@outlook.com>
2024-01-24 23:19:00 +0300

    Order MediatR/Command/CommandHandle

---
+++ Services/Order/Course.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,17 @@
+﻿using Course.Services.Order.Application.Dtos;
+using Course.Shared.Dtos;
+using MediatR;
+using System.Collections.Generic;
+
+namespace Course.Services.Order.Application.Queries
+{
+    class CreateOrderCommand : IRequest<Response<CreatedOrderDto>>
+    {
+        public string BuyerId { get; set; }
+
+        public List<OrderItemDto> OrderItems { get; set; }
+
+        public AddressDto Address { get; set; }
+
+    }
+}
---
+++ Services/Order/Course.Services.Order.Application/Handlers/CreateOrderHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Course.Services.Order.Application.Dtos;
+using Course.Services.Order.Application.Mapping;
+using Course.Services.Order.Application.Queries;
+using Course.Services.Order.Domain.OrderAggregate;
+using Course.Services.Order.Infrastructure;
+using Course.Shared.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Course.Services.Order.Application.Handlers
+{
+    class CreateOrderHandler : IRequestHandler<CreateOrderCommand, Response<CreatedOrderDto>>
+    {
+        private readonly OrderDbContext _dbContext;
+
+        public CreateOrderHandler(OrderDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var address = ObjectMapper.Mapper.Map<Address>(request.Address);
+
+            var order = new Domain.OrderAggregate.Order(address, request.BuyerId);
+
+            request.OrderItems.ForEach(x =>
+            {
+                order.AddOrderItem(x.ProductId, x.ProductName, x.PictureUrl, x.Price);
+            });
+
+            await _dbContext.Orders.AddAsync(order);
+            await _dbContext.SaveChangesAsync();
+
+            return Response<CreatedOrderDto>.Success(new CreatedOrderDto { Id = order.Id }, 200);
+        }
+    }
+}
