3f1ea442c9178a12da0d5ab46044a958689b5d52
hamdiefe <hamdi.efe@outlook.com>
2024-02-04 21:50:06 +0300

    IdentityService Signin

--- Frontends/Course.Web/Services/IdentityService.cs
+++ Frontends/Course.Web/Services/IdentityService.cs
@@ -2,10 +2,18 @@
 using Course.Web.Models;
 using Course.Web.Services.Interfaces;
 using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Globalization;
+using System;
 using System.Net.Http;
+using System.Security.Claims;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace Course.Web.Services
 {
@@ -37,9 +45,70 @@ namespace Course.Web.Services
 			throw new System.NotImplementedException();
 		}
 
-		public Task<Response<bool>> SignIn(SignInInput signInInput)
+		public async Task<Response<bool>> SignIn(SignInInput signInInput)
 		{
-			throw new System.NotImplementedException();
+			var discovery = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+			{
+				Address = _serviceApiSettings.BaseUrl,
+				Policy = new DiscoveryPolicy { RequireHttps =false}
+			});
+
+			if (discovery.IsError)			
+				throw discovery.Exception;
+
+			var passwordTokenRequest = new PasswordTokenRequest
+			{
+				ClientId = _clientSettings.WebClientForUser.ClientId,
+				ClientSecret = _clientSettings.WebClientForUser.ClientSecret,
+				UserName = signInInput.Email,
+				Password = signInInput.Password,
+				Address = discovery.TokenEndpoint
+			};
+
+
+			var token = await _httpClient.RequestPasswordTokenAsync(passwordTokenRequest);
+
+			if (token.IsError)
+			{
+				var responseContent = await token.HttpResponse.Content.ReadAsStringAsync();
+
+				var errorDto = JsonSerializer.Deserialize<ErrorDto>(responseContent,new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+
+				return Response<bool>.Fail(errorDto.Errors,404);
+			}
+
+			var userInfoRequest = new UserInfoRequest
+			{
+				Token = token.AccessToken,
+				Address = discovery.UserInfoEndpoint
+			};
+
+			var userInfo = await _httpClient.GetUserInfoAsync(userInfoRequest);
+
+            if (userInfo.IsError)
+            {
+				throw userInfo.Exception;
+            }
+
+
+			var claimsIdentity = new ClaimsIdentity(userInfo.Claims, CookieAuthenticationDefaults.AuthenticationScheme, "name", "role");
+			var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+			var authenticationProperties = new AuthenticationProperties();
+
+			authenticationProperties.StoreTokens(new List<AuthenticationToken>()
+			{
+				new AuthenticationToken{ Name=OpenIdConnectParameterNames.AccessToken,Value=token.AccessToken},
+				new AuthenticationToken{ Name=OpenIdConnectParameterNames.RefreshToken,Value=token.RefreshToken},
+		        new AuthenticationToken{ Name=OpenIdConnectParameterNames.ExpiresIn,Value= DateTime.Now.AddSeconds(token.ExpiresIn).ToString("o",CultureInfo.InvariantCulture)}
+			});
+
+			authenticationProperties.IsPersistent = signInInput.RememberMe;
+			await _contextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, authenticationProperties);
+
+			return Response<bool>.Success(200);
+
+
 		}
 	}
 }
