3f0bdcd88c417dce701424f86d5734dfec1a31e8
hamdiefe <hamdi.efe@outlook.com>
2024-02-07 21:19:36 +0300

    ServiceExtension Conteiner ve Basket Service

---
+++ Frontends/Course.Web/Services/BasketService.cs
@@ -0,0 +1,119 @@
+﻿using Course.Shared.Dtos;
+using Course.Web.Models.Basket;
+using Course.Web.Services.Interfaces;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Course.Web.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddBasketItem(BasketItemViewModel basketItemViewModel)
+        {
+            var basket = await Get();
+
+            if (basket != null)
+            {
+                if (!basket.BasketItems.Any(x => x.CourseId == basketItemViewModel.CourseId))
+                {
+                    basket.BasketItems.Add(basketItemViewModel);
+                }
+            }
+            else
+            {
+                basket = new BasketViewModel();
+
+                basket.BasketItems.Add(basketItemViewModel);
+            }
+
+            await SaveOrUpdate(basket);
+        }
+
+        public Task<bool> ApplyDiscount(string discountCode)
+        {
+            throw new System.NotImplementedException();
+        }   
+
+        public async Task<bool> CancelApplyDiscount()
+        {
+            var basket = await Get();
+
+            if (basket == null || basket.DiscountCode == null)
+            {
+                return false;
+            }
+
+            basket.CancelDiscount();
+            await SaveOrUpdate(basket);
+            return true;
+        }
+
+        public async Task<bool> Delete()
+        {
+            var result = await _httpClient.DeleteAsync("baskets");
+
+            return result.IsSuccessStatusCode;
+        }
+
+        public async Task<BasketViewModel> Get()
+        {
+            var response = await _httpClient.GetAsync("baskets");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            var basketViewModel = await response.Content.ReadFromJsonAsync<Response<BasketViewModel>>();
+
+            return basketViewModel.Data;
+        }
+
+        public async Task<bool> RemoveBasketItem(string courseId)
+        {
+            var basket = await Get();
+
+            if (basket == null)
+
+            {
+                return false;
+            }
+
+            var deleteBasketItem = basket.BasketItems.FirstOrDefault(x => x.CourseId == courseId);
+
+            if (deleteBasketItem == null)
+            {
+                return false;
+            }
+
+            var deleteResult = basket.BasketItems.Remove(deleteBasketItem);
+
+            if (!deleteResult)
+            {
+                return false;
+            }
+
+            if (!basket.BasketItems.Any())
+            {
+                basket.DiscountCode = null;
+            }
+
+            return await SaveOrUpdate(basket);
+        }
+
+        public async Task<bool> SaveOrUpdate(BasketViewModel basketViewModel)
+        {
+            var response = await _httpClient.PostAsJsonAsync<BasketViewModel>("baskets", basketViewModel);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
--- Frontends/Course.Web/Startup.cs
+++ Frontends/Course.Web/Startup.cs
@@ -1,4 +1,5 @@
 using Course.Shared.Services;
+using Course.Web.Extensions;
 using Course.Web.Handler;
 using Course.Web.Helpers;
 using Course.Web.Models;
@@ -39,21 +40,7 @@ namespace Course.Web
             services.AddScoped<ISharedIdentityService, SharedIdentityService>();
             services.AddSingleton<PhotoHelper>();
 
-            var serviceApiSettings = Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
-            services.AddHttpClient<IClientCredentialTokenService, ClientCredentialTokenService>();
-            services.AddHttpClient<IIdentityService, IdentityService>();
-            services.AddHttpClient<IUserService, UserService>(opt =>
-            {
-                opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUrl);
-            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
-            services.AddHttpClient<ICatalogService, CatalogService>(opt =>
-            {
-                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUrl}/{serviceApiSettings.Catalog.Path}");
-            }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
-            services.AddHttpClient<IPhotoStockService, PhotoStockService>(opt =>
-            {
-                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUrl}/{serviceApiSettings.PhotoStock.Path}");
-            }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
+            services.AddHttpClientServices(Configuration);
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, opts =>
             {
