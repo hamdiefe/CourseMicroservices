a20c2f00c669033ebbbd0b9320a980bfa0ab06f2
hamdiefe <hamdi.efe@outlook.com>
2024-02-07 20:56:32 +0300

    Home controller düzenlemesi

--- Frontends/Course.Web/Controllers/HomeController.cs
+++ Frontends/Course.Web/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using Course.Web.Models;
+﻿using Course.Web.Exceptions;
+using Course.Web.Models;
+using Course.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Course.Web.Controllers
@@ -13,24 +13,36 @@ namespace Course.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ICatalogService _catalogService;
+
+        public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
         {
             _logger = logger;
+            _catalogService = catalogService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var model = await _catalogService.GetAllCourseAsync();
+            return View(model);
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Detail(string id)
         {
-            return View();
+            var model = await _catalogService.GetByCourseId(id);
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            var errorFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            if (errorFeature != null && errorFeature.Error is UnAuthorizeException)
+            {
+                return RedirectToAction(nameof(AuthController.Logout), "Auth");
+            }
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
