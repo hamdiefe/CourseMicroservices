712c76f4d6b9884e676e735656f75f0c83e6de28
hamdiefe <hamdi.efe@outlook.com>
2024-02-09 21:57:58 +0300

    Payment RabbitMQ MassTransit

--- Services/Payment/Course.Services.Payment/Startup.cs
+++ Services/Payment/Course.Services.Payment/Startup.cs
@@ -1,3 +1,4 @@
+using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
@@ -29,6 +30,22 @@ namespace Course.Services.Payment
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMassTransit(x=>
+            {
+                //Default port:5672
+                x.UsingRabbitMq((context,cfg) =>
+                {
+                    cfg.Host(Configuration["RabbitMqUrl"], "/", host =>
+                    {
+                        host.Username("guest");
+                        host.Password("guest");
+                    });
+                });
+            });
+
+
+            services.AddMassTransitHostedService();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
                 options.Authority = Configuration["IdentityServerUrl"];
